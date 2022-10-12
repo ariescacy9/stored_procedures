6c6b5c28b9c7f8be6cb290d6ad35915a73039a3e
tristan99 <ariescacy9@gmail.com>
2022-10-12 12:24:35 -0500

    change 2 procedures

--- productos.sql
+++ productos.sql
@@ -12,30 +12,44 @@ create table inventory (
   amount_total float
 );
 
+ --- procedimiento compra en inventario
+ create or replace procedure buy_products(
+   products_id int, 
+   amount_buy float
+)
+language plpgsql    
+as $$
+begin
+    -- subtracting the amount from the sender's account 
+    update inventory 
+    set amount_total  = amount_total  + amount_buy
+    where id = products_id;
+
+    commit;
+end;$$
+
 --- procedimiento venta en inventario
-create or replace procedure buy_or_sell(
+create or replace procedure sell_products(
   products_id int, 
-  amount_buy float,
   amount_sale float
 )
 language plpgsql    
 as $$
 begin
-  -- se aumenta una cantidad si se compran mas unidades del producto en inventario
-  update inventory 
-  set amount_total  = amount_total  + amount_buy
-  where id = products_id;
-
-  -- ase disminuye una cantidad si se venden las unidades del producto en inventario
-  update inventory
-  set amount_total = amount_total - amount_sale
-  where id = products_id;
+    -- adding the amount to the receiver's account
+    update inventory
+    set amount_total = amount_total - amount_sale
+    where id = products_id;
 
     commit;
 end;$$
 
+
+---drop procedure buy_or_sell;
+
 --- se hace el llamado al procedimiento
-call buy_or_sell(1, 0, 50);
+call buy_products(1, 50);
+call sell_products(1, 23);
 
 --- se verifica que se hicieron los cambios en la tabla inventory del producto con id=1 restando 50 unidades
 SELECT * FROM inventory;
